a3f11839e9447f7f17ebcff6c825732ae5ca243e
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-26 13:27:00 +0100

    Refactor

--- delete.php
+++ delete.php
@@ -1,2 +1,13 @@
 <?php
 /**** Supprimer une randonnée ****/
+  require "./config.php";
+
+  if (isset($_GET['id'])){
+    $db_conn = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpassword);
+    $db_conn->query("DELETE FROM hiking WHERE id=$_GET[id]");
+
+  }
+
+  $redirect = "http://".$_SERVER["HTTP_HOST"]."/sql/randonnee/read.php";    
+  header("Location:$redirect");
+?>
