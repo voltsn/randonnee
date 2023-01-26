c56a9189c67ba5af60d7f793fb6a5e8b0f10bc4b
Nntanisimine Volts <voltsnntanisimine@gmail.com>
2023-01-26 09:37:04 +0100

    Fix broken link

--- create.php
+++ create.php
@@ -8,6 +8,8 @@
 		$dur = $_POST["duration"];
 		$height_diff = $_POST["height_difference"];
 
+		// Convert to meters
+		$dist = intval($dist) * 1000;
 			
 		$query_status;
 		try{
@@ -41,7 +43,7 @@
 	<link rel="stylesheet" href="css/basics.css" media="screen" title="no title" charset="utf-8">
 </head>
 <body>
-	<a href="/php-pdo/read.php">Liste des données</a>
+	<a href="./read.php">Liste des données</a>
 	<h1>Ajouter</h1>
 	<?php 
 		if (isset($query_status)){
